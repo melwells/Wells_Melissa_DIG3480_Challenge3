e87cfead2f2aa5ea9cb849b117938cd0f831c88e
melwells <55035851+melwells@users.noreply.github.com>
2019-11-15 17:30:42 -0500

    First

---
+++ Wells_Melissa_DIG3480_Tutorial3/Assets/Scripts/GameController.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+  public GameObject [] hazards;
+  public Vector3 spawnValues;
+  public int hazardCount;
+  public float spawnWait;
+  public float startWait;
+  public float waveWait;
+
+  public Text pointsText;
+  public Text restartText;
+  public Text gameOverText;
+
+  private int points;
+  private bool gameOver;
+  private bool restart;
+
+  void Start()
+  {
+    points = 0;
+    gameOver = false;
+    restart = false;
+    restartText.text = "";
+    gameOverText.text = "";
+    UpdateScore();
+    StartCoroutine(SpawnWaves());
+  }
+
+  void Update()
+  {
+      if (Input.GetKey("escape"))
+      {
+        Application.Quit();
+      }
+
+      if (restart)
+      {
+        if (Input.GetKeyDown (KeyCode.Return))
+        {
+          SceneManager.LoadScene("Scene1");
+        }
+      }
+  }
+
+  IEnumerator SpawnWaves()
+  {
+    yield return new WaitForSeconds(startWait);
+    while (true)
+    {
+      for (int i = 0; i < hazardCount; i++)
+      {
+        GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(spawnWait);
+      }
+      yield return new WaitForSeconds(waveWait);
+      if (gameOver)
+      {
+        restartText.text = "Press Enter to Restart";
+        restart = true;
+        break;
+      }
+    }
+  }
+
+  public void AddScore(int newScoreValue)
+  {
+    points += newScoreValue;
+    UpdateScore();
+  }
+
+  void UpdateScore()
+  {
+    pointsText.text = "Points: " + points;
+    if (points >= 100)
+    {
+      gameOverText.text = "You Win! Game created by Melissa Wells.";
+      gameOver = true;
+      restart = true;
+    }
+  }
+
+  public void GameOver()
+  {
+    gameOverText.text = "Game Over!";
+    gameOver = true;
+  }
+}
